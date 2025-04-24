bd29e574e85ddbc8241ca15cc38845494695dd4f
Apparat4ik <apparat.inc45@gmail.com>
2025-04-24 20:30:53 +0700

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Владислав on 24.04.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
