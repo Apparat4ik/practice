c10d01bcfd365164fe6acf3af5ba2cab5ec2cadf
Apparat4ik <apparat.inc45@gmail.com>
2025-05-17 00:39:52 +0700

    a^x mod p (2 algorithms)

--- practice/main.cpp
+++ practice/main.cpp
@@ -1,14 +1,1 @@
-//
-//  main.cpp
-//  practice
-//
-//  Created by Владислав on 24.04.2025.
-//
 
-#include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
