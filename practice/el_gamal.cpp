88653bcf0a80accc577cc0e9623fe4c61c7cbd84
Apparat4ik <apparat.inc45@gmail.com>
2025-08-19 03:08:46 +0700

    stable version

--- practice/el_gamal.cpp
+++ practice/el_gamal.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
 #include <stdint.h>
 #include <random>
-
+#include <cmath>
+#include <map>
+#include <sstream>
+#include <iomanip>
+#include <uchar.h>
 
 using namespace std;
 
@@ -10,10 +15,27 @@ using namespace std;
 random_device rd;
 mt19937 gen(rd());
 
+vector<pair<uint64_t, uint64_t>> Cypher_text;
 
 
+map<char, uint64_t> Public_Key = {};
+
+uint64_t Private_Key;
+
+uint64_t Session_Key;
 
 vector<uint64_t> pr_num;
+
+
+
+
+ostream& operator<<(ostream& os, vector<pair<uint64_t, uint64_t>> vc){
+    for (pair<uint64_t, uint64_t> pr : vc){
+        os << pr.first << ' ' << pr.second << ' ';
+    }
+    return os;
+}
+
 
 vector<uint64_t> Sieve(int n) {                 // нахождение простых чисел через решето Эратосфена
     vector<bool> is_prime(n + 1, true);
@@ -120,13 +142,13 @@ bool MillerTest(uint64_t n, int t) {
 }
 
 
-uint64_t Just_P_Gen(int k, const int& t) {
+uint64_t P_Gen(int k, const int& t) {
     uint64_t n_min = 1 << (k - 2), n_max = (1 << (k - 1)) - 1;
     uint64_t p = 0, m = 1;
     while (!MillerTest(p, t)) {
         m = 1;
         while (!(n_min < m && m < n_max) && (m > 0)) {
-            int32_t primeIt = gen() % pr_num.size();
+            int primeIt = gen() % pr_num.size();
             uint64_t primeExp = gen() % 3;
             m *= pow(pr_num[primeIt], primeExp);
             while (m >= n_max) {
@@ -144,28 +166,111 @@ uint64_t Just_P_Gen(int k, const int& t) {
 }
 
 
-pair<uint64_t, uint64_t> P_and_G_Gen(const int& k,const int& t){
-    uint64_t p = Just_P_Gen(20, 10);
-    uint64_t q = (p - 1) / 2, g = 0;
-    do {
-        if (!MillerTest(q, 10)){
-            p = Just_P_Gen(k, t);
-            q = (p - 1) / 2;
-        }
-        g = gen() % (p - 1);
-    } while (Mod_pow(g, q, p) == 1);
-    cout << Mod_pow(g, q, p) << endl;
-    return {p, g};
+
+bool G_is_PrimitiveRoot(uint64_t g, uint64_t p){
+    if (gcd(g, p) != 1){return false;}
+    if (Mod_pow(g, (p - 1) / 2, p) == 1){return false;}
+    
+    uint64_t L = 0;
+    for (pair<uint64_t, uint64_t> factor : Decomposition(p - 1)){
+        L = (p - 1) / factor.first;
+        if (Mod_pow(g, L, p) == 1){return false;}
+    }
+    return true;
 }
 
+
+uint64_t G_Gen(uint64_t p){
+    uint64_t g = 0;
+    do {
+        g = gen() % (p - 1);
+    } while (!G_is_PrimitiveRoot(g, p));
+    return g;
+}
+
+void GenKeys(int k, int t){
+    
+    uint64_t p = P_Gen(k, t);
+    cout << "Число p: " << p << endl;
+    
+    uint64_t g = G_Gen(p);
+    cout << "Число g: " << g << endl;
+    
+    uint64_t x = 1 + (gen() % (p - 2));
+    cout << "Число x: " << x << endl;
+    
+    uint64_t y = Mod_pow(g, x, p);
+    cout << "Число y: " << y << endl;
+    
+    Public_Key['y'] = y;
+    Public_Key['g'] = g;
+    Public_Key['p'] = p;
+    
+    Private_Key = x;
+}
+
+
+
+void Encryption(const uint64_t& m){
+    uint64_t a = Mod_pow(Public_Key['g'], Session_Key, Public_Key['p']);
+    uint64_t b1 = Mod_pow(Public_Key['y'], Session_Key, Public_Key['p']);
+    uint64_t b2 = Mod_pow(m, 1, Public_Key['p']);
+    uint64_t b = Mod_pow(b1 * b2, 1, Public_Key['p']);
+    Cypher_text.push_back({a, b});
+}
+
+
+
+string M_to_text(uint64_t& m, string& plain_text){
+    string m_str = to_string(m);
+    for (int i = 0; i < m_str.size(); i += 3){
+        
+        string ltr;
+        ltr.push_back(m_str[i]);
+        ltr.push_back(m_str[i + 1]);
+        ltr.push_back(m_str[i + 2]);
+        
+        plain_text += stoi(ltr);
+    }
+    return plain_text;
+}
+
+void Decryption(string& plain_text){
+    for (pair<uint64_t, uint64_t> cypher : Cypher_text){
+        uint64_t m1 = Mod_pow(cypher.first, Public_Key['p'] - 1 - Private_Key, Public_Key['p']);
+        uint64_t m2 = Mod_pow(cypher.second, 1, Public_Key['p']);
+        uint64_t m = Mod_pow(m1 * m2, 1, Public_Key['p']);
+        M_to_text(m, plain_text);
+    }
+    Cypher_text.clear();
+}
 
 
 
 
 int main(){
     Sieve(500);
-    pair<uint64_t, uint64_t> p_and_g = P_and_G_Gen(20, 10);
+    GenKeys(20, 10);
+    Session_Key = 1 + (gen() % (Public_Key['p'] - 2));
+    string plain_text;
+    cout << "Введите текст, который хотите сашифровать" << endl;
+    getline(cin, plain_text);
+    
+    for (uint8_t ltr : plain_text){
+        Encryption(ltr);
+        plain_text.erase(0);
+    }
+   
+    cout << "зашифрованное сообшение: " << endl;
+    cout << Cypher_text << endl;
+    
+    cout << "Расшифрованное сообщение: " << endl;
+    
+    Decryption(plain_text);
+    cout << plain_text << endl;
 
+    
+    
     
     
     return 0;
